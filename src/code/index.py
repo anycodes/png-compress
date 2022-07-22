2a13fc9fec855591610c676ef79d77c996da382f
rsonghuster <ls_huster@163.com>
2022-07-22 17:55:49 +0800

    png use pngquant

--- src/code/index.py
+++ src/code/index.py
@@ -4,6 +4,7 @@ import oss2
 import json
 import os
 from wand.image import Image
+import subprocess
 
 
 def handler(event, context):
@@ -22,8 +23,17 @@ def handler(event, context):
     new_image_path = '/tmp/new_{}'.format(filename)
     bucket.get_object_to_file(image, image_path)
     quality = evt['quality']
-    with Image(filename=image_path) as img:
-        img.compression_quality = quality
-        img.save(filename=new_image_path)
+    (fn, extension) = os.path.splitext(filename)
+    if extension == '.png':
+        subprocess.check_call('./pngquant --quality {}-{} {}'.format(
+            int(quality*0.75), int(quality*1.2), image_path), shell=True)
+        new_image_path = '/tmp/{}-fs8.png'.format(fn)
+    else:
+        with Image(filename=image_path) as img:
+            img.compression_quality = quality
+            img.save(filename=new_image_path)
     dst = evt['dst']
     bucket.put_object_from_file(os.path.join(dst, filename), new_image_path)
+
+    os.remove(image_path)
+    os.remove(new_image_path)
